c7ff59a661b35a765ad57a488ead4279c40128de
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-05-17 10:32:58 +0300

    fix __init__ method

--- src/classes.py
+++ src/classes.py
@@ -3,14 +3,14 @@ class Operation:
     Абстракция операции.
     """
 
-    def __init__(self, id_tr):
+    def __init__(self, id_tr: int):
         """
         Инициализация класа.
         :param id_tr: id операции обязателен
         """
-        self.id = id_tr
+        self.id: int = id_tr
         self.state: str = ''  # EXECUTED` — выполнена, `CANCELED` — отменена.
-        self.date: str = ''
+        self.date_t = None
         self.op_am = {}  # сумма операции и валюта
         self.descr: str = ''  # описание типа перевода
         self.from_: str = ''  # откуда(может отсутстовать)
@@ -20,7 +20,7 @@ class Operation:
         data = [
             f'id:{self.id}',
             f'state:{self.state}',
-            f'date:{self.date}',
+            f'datetime:{self.date_t}',
             f'op_am:{self.op_am}',
             f'descr:{self.descr}',
             f'from_:{self.from_}',
@@ -32,7 +32,7 @@ class Operation:
         data = [
             f'{self.id}',
             f'{self.state}',
-            f'{self.date}',
+            f'{self.date_t}',
             f'{self.op_am}',
             f'{self.descr}',
             f'{self.from_}',
