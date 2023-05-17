687be5ec724032c4e3332b63dd320a53b9221652
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-05-17 11:12:25 +0300

    add test to get_client func.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,29 @@
-from src.utils import read_operations
+from src.utils import read_operations, get_client
 from src.constants import FILE_NAME
-
+from src.classes import Client
 
 def test_read_operations():
     assert read_operations('.', '1234') == []
     assert read_operations('.', FILE_NAME) != []
+
+
+def test_get_client():
+    data = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
+    cl = get_client([data])
+    cl2 = get_client([{}])
+    assert isinstance(cl, Client)
+    assert cl2.operations == []
