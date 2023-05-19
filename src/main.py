59777b36172f0c864a677affc6ca5951382e87af
Vitaliy Petrenko <mail@v-petrenko.ru>
2023-05-19 20:03:09 +0300

    FIX order print

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ from src.utils import read_operations, get_client, operation_to_stdout
 from src.constants import FILE_NAME, NUMBER_OF_OPERATIONS
 COUNT = 5
 
-data1 = read_operations('..', FILE_NAME)
+data1 = read_operations('..', FILE_NAME)  # Читаем файлик в список
 
-cl = get_client(data1)
+cl = get_client(data1)  # Инициируем клиента с данными из шага выше
 
-for i in range(NUMBER_OF_OPERATIONS):
+data = [cl.get_operation() for i in range(NUMBER_OF_OPERATIONS)]  # list из операций
+revers_data = data[::-1]  # Реверс этого листа
+
+for i in revers_data:  # Печать этого листа через пустую строку
     print()
-    operation_to_stdout(cl.get_operation())
+    operation_to_stdout(i)
